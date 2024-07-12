6a1b7715078b8a3e1be69bb5abb27d719726701a
matze328 <152269645+matze328@users.noreply.github.com>
2024-07-12 13:49:30 +0200

    init users
    user route

---
+++ src/database/models/UserModel.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Model } from "sequelize";
+import todoSequelize from "../setup/database";
+import { UserAttributes } from "../../interfaces/db-models/UserAttributes";
+
+// Define the User model
+class UserModel extends Model implements UserAttributes{
+  
+  public id!: number;
+  public email!: string;
+  public name!: string;
+  public password!: string;
+  public profileImgUrl?: string;
+
+  static initializeModel() {
+    UserModel.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        profileImgUrl: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+      },
+      {
+        sequelize: todoSequelize,
+        tableName: "Users",
+        defaultScope: {
+          attributes: { exclude: ["password"] },
+        },
+       
+      }
+    );
+  }
+}
+
+UserModel.initializeModel();
+
+export default UserModel;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import TodosRouter from "./todos";
+import UserRouter from "./users";
 
 const AppRouter = Router();
 
 AppRouter.use("/todos", TodosRouter);
-
+AppRouter.use("/users", UserRouter);
 export default AppRouter;
