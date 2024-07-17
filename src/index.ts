85612f8d2ca15d5041a131b4a92a78f8bd925f19
matze328 <152269645+matze328@users.noreply.github.com>
2024-07-17 16:27:16 +0200

    fehler behoben

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import express from 'express';
 import todoSequelize from './database/setup/database';
 import app from './server';
-
+import swaggerUi from 'swagger-ui-express';
 // Zugriff auf Umgebungsvariablen
 // const { PORT } = process.env;
 const PORT = process.env.PORT;
@@ -13,6 +14,23 @@ todoSequelize
   .catch(e => {
     console.log(e);
   });
+  if (process.env.NODE_ENV === 'dev') {
+    app.use(express.static('docs'));
+    app.use(
+      '/swagger',
+      swaggerUi.serve,
+      swaggerUi.setup(undefined, {
+        swaggerOptions: {
+          url: '/swagger.json',
+        },
+      }),
+    );
+    console.log(
+      `Swagger launched on at https://localhost:${
+        process.env.PORT ?? ''
+      }/swagger`,
+    );
+  }
 
 // App hört im folgenden auf den Port, welcher über die Umgebungsvariable definiert ist
 app.listen(PORT, () => {
